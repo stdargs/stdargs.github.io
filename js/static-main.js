2427f7a0559ba8b4c16b50973bd4017856ff3520
maitysubhasis <stdargs@gmail.com>
2019-04-27 17:56:39 +0530

    added scroll hide navbar

--- js/static-main.js
+++ js/static-main.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-  
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementsByClassName("main-nav")[0].style.top = "0";
+    } else {
+      document.getElementsByClassName("main-nav")[0].style.top = "-58px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
 });
